914adf933d2ded478491b95284f016638f7a1581
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-10-01 23:09:04 +0200

    Add form elements and click event to form element.

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// All elements
+const form = document.querySelector('film-form');
+const titleElement = document.querySelector('#title');
+const directorElement = document.querySelector('#director');
+const urlElement = document.querySelector('#url');
+
+// All Events
+function eventListeners(){
+    form.addEventListener('click', addFilm);
+}
+
+
+////// Functions //////
+
+function addFilm(e){
+
+}
