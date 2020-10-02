76f980c730f696445189431910646a6b91e51ab3
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-10-02 22:37:01 +0200

    Add get() and add() functions

--- js/storage.js
+++ js/storage.js
@@ -1,3 +1,26 @@
 function Storage() {
     
 }
+
+Storage.prototype.add = function(newFilm){
+    let films = this.get();
+
+    const index = films.findIndex((film) => newFilm.title == film.title);
+    
+    if(index === -1){
+        films.push(newFilm);
+        localStorage.setItem('films', JSON.stringify(films));
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+Storage.prototype.get = function(){
+    let films;
+    if(localStorage.getItem('films') === null) films = [];
+    else films = JSON.parse(localStorage.getItem('films'));
+    return films;
+
+}
