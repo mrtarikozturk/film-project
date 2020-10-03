9fcef737ababcb0f3ab499c1233e1eaed18215b8
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-10-03 12:23:47 +0200

    Add clearAllFilms function

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@ function eventListeners(){
     form.addEventListener('submit', addFilm);
     document.addEventListener('DOMContentLoaded', loadAllFilms);
     secondCardBody.addEventListener('click', deleteFilm);
+    clearAllButton.addEventListener('click', clearAllFilms);
+        
         
 }
 
@@ -50,7 +52,6 @@ function addFilm(e){
 
 function loadAllFilms(){
     let films = storage.get();
-    console.log(films);
     ui.loadAll(films);
 }
 
@@ -59,12 +60,16 @@ function deleteFilm(e) {
 
         const filmElement = e.target.parentElement.parentElement;
         ui.delete(filmElement);
-        console.log(filmElement);
 
         const filmName = filmElement.children[1].textContent;
         storage.delete(filmName);
-        console.log(filmName);
         ui.displayMessage('success', 'Film successfully deleted...');
     }
 }
 
+function clearAllFilms() {
+    if(confirm('Are you sure you want to delete all films?')){
+        ui.clearAll();
+        storage.clearAll();
+    }
+}
